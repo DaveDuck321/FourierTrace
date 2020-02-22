fff7ebcb2ebdc8f1054460976c43c68a9bb58c0e
Thomas Grant <thomas.grant.duck321@gmail.com>
2020-02-22 00:24:23 +0000

    Pre generate focal points

--- render.py
+++ render.py
@@ -35,7 +35,7 @@ def get_pixels(POINTS):
     PATH = unwrap_path.linear_extrapolater(POINTS)
 
     series = fourier_series(PATH, PERIOD)
-    terminating = list(islice(series, 20))
+    terminating = list(islice(series, 10))
 
     RESOLUTION = 1000
     return map(
@@ -48,10 +48,14 @@ def get_pixels(POINTS):
 
 def gen_draw_pendulum(lifetime=1):
     trail = []
-    def draw_pendulum(camera, accumulation):
+    def draw_pendulum(camera, accumulation, focus):
         for p, c in zip(accumulation, accumulation[1:]):
             camera.add_shape( Line((255, 255, 255), p, c) )
-            camera.add_shape( Circle((0, 255, 0), p, abs(p-c)) )
+
+            if p == accumulation[focus]:
+                camera.add_shape( Circle((0, 255, 0), p, abs(p-c)) )
+            else:
+                camera.add_shape( Circle((0, 50, 255), p, abs(p-c)) )
 
         current_t = time.time()
         trail.append((current_t, accumulation[-1]))
@@ -62,7 +66,7 @@ def gen_draw_pendulum(lifetime=1):
 
         for (created, p1), (_, p2) in zip(trail, trail[1:]):
             intensity = 255 - int(255*(current_t-created)/ lifetime)
-            camera.add_shape( Line((0, 0, intensity), p1, p2), -created)
+            camera.add_shape( Line((intensity, 0, 0), p1, p2), -created)
 
     return draw_pendulum
 
@@ -82,6 +86,21 @@ def gen_radial_accumulation(POINTS):
         ))
 
     return radial_accumulation
+
+def get_focal_points(accumulation):
+    local_radii = list(map(
+        lambda p: abs(p[0]-p[1]),
+        zip(accumulation, accumulation[1:])
+    ))
+    global_radii = [0]+list(accumulate(local_radii))
+
+    return list(map(
+        lambda node: ( node[0], global_radii[-1]-global_radii[node[0]] ),
+        filter(
+            lambda node: node[1] > 0.01,
+            enumerate(local_radii)
+        )
+    ))
 
 def main():
     TEST_PATH = []
@@ -106,28 +125,37 @@ def main():
     d_time = 1/60
     running = True
 
+    focus = 0
+    focal_points = get_focal_points(radial_accumulation(0))
+
     rotation = 0
     while running:
         # Frame logic
         t = timer()
-        rotation += d_time
+        # Dilate time while zoomed in -- match rotation speed
+        rotation += d_time / ((focus+3)//2)
         accumulation = radial_accumulation(rotation)
 
         # Drawing
         screen.fill((0, 0, 0))
-        draw_pendulum(camera, accumulation)
+        draw_pendulum(camera, accumulation, focal_points[focus][0])
 
-        camera.center = accumulation[0]
-        camera.radius = 1.5
+        camera.center = accumulation[focal_points[focus][0]]
 
         camera.flush()
         pygame.display.flip()
-
 
         # Timing
         d_time = t()
         # Events
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    focus = min(focus+1, len(focal_points)-1)
+                    camera.radius = focal_points[focus][1]
+                if event.key == pygame.K_LEFT:
+                    focus = max(0, focus-1)
+                    camera.radius = focal_points[focus][1]
             if event.type == pygame.QUIT:
                 running = False
 
